2a9a72bc9bc2973fe77eda8bb75785aef7bdf2c7
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-14 20:31:32 +0200

    instantiate and configure fastapi app and add serve function to run uvicorn

--- docaudit/__init__.py
+++ docaudit/__init__.py
@@ -12,3 +12,62 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .utils import to_abs_path
+
+from . import database
+from .angular import AngularFiles
+from .config import load_config
+from .endpoints import project_router
+
+config = load_config()
+app = FastAPI(
+    title="DocAudit API",
+    docs_url=config.fastapi.docs_url,
+    redoc_url=config.fastapi.redoc_url,
+)
+
+app.include_router(project_router, prefix="/api")
+app.mount("/", AngularFiles(directory=to_abs_path("htdocs"), html=True))
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000", "http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+def on_startup():
+    # migration.migrate(config.database)
+    database.setup_connection(config.database)
+    database.create_all()
+
+
+@app.on_event("shutdown")
+def on_shutdown():
+    database.dispose_connection()
+
+
+def serve():
+    uvicorn.run(
+        "docaudit:app",
+        host=config.uvicorn.host,
+        port=config.uvicorn.port,
+        reload=config.uvicorn.reload,
+        log_level=config.uvicorn.log_level,
+        log_config=config.uvicorn.log_config,
+        ssl_keyfile=config.uvicorn.ssl_keyfile,
+        ssl_certfile=config.uvicorn.ssl_certfile,
+        ssl_keyfile_password=config.uvicorn.ssl_keyfile_password,
+        ssl_version=config.uvicorn.ssl_version,
+        ssl_cert_reqs=config.uvicorn.ssl_cert_reqs,
+        ssl_ca_certs=config.uvicorn.ssl_ca_certs,
+        ssl_ciphers=config.uvicorn.ssl_ciphers,
+    )
