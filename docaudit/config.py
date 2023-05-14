2e59fd1ac74106158798baa060110e8b3c0721a8
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-14 16:07:43 +0200

    extend config to configure fastapi and uvicorn

--- docaudit/config.py
+++ docaudit/config.py
@@ -15,8 +15,10 @@
 
 from functools import lru_cache
 import yaml
+import ssl
 from pydantic import BaseModel
-from docaudit.utils import to_abs_path
+from uvicorn.config import LOGGING_CONFIG, SSL_PROTOCOL_VERSION
+from .utils import to_abs_path
 
 
 class DatabaseConfig(BaseModel):
@@ -24,8 +26,56 @@ class DatabaseConfig(BaseModel):
     echo: bool = False
 
 
+class FastApiConfig(BaseModel):
+    docs_url: str | None = None  # "/docs"
+    redoc_url: str | None = None  # "/redoc"
+
+
+class UvicornConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+    reload: bool = False
+    log_level: str = "error"
+    log_filename: str | None = None
+    ssl_keyfile: str | None = None
+    ssl_certfile: str | None = None
+    ssl_keyfile_password: str | None = None
+    ssl_version: int = SSL_PROTOCOL_VERSION
+    ssl_cert_reqs: int = ssl.CERT_NONE
+    ssl_ca_certs: str | None = None
+    ssl_ciphers: str = "TLSv1"
+
+    @property
+    def log_config(self) -> dict:
+        if not self.log_filename:
+            return LOGGING_CONFIG
+
+        custom_logging_config = LOGGING_CONFIG.copy()
+        custom_logging_config["formatters"]["default"]["use_colors"] = False
+        custom_logging_config["formatters"]["access"]["use_colors"] = False
+        custom_logging_config["handlers"] = {
+            "default": {
+                "class": "logging.FileHandler",
+                "formatter": "default",
+                "filename": self.log_filename,
+                "mode": "a",
+                "encoding": "utf-8",
+            },
+            "access": {
+                "class": "logging.FileHandler",
+                "formatter": "access",
+                "filename": self.log_filename,
+                "mode": "a",
+                "encoding": "utf-8",
+            },
+        }
+        return custom_logging_config
+
+
 class Config(BaseModel):
     database: DatabaseConfig = DatabaseConfig()
+    fastapi: FastApiConfig = FastApiConfig()
+    uvicorn: UvicornConfig = UvicornConfig()
 
 
 CONFIG_FILENAME = "config.yml"
