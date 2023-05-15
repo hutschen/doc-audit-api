0d268caa366eab1d230aaaddae65f55500527ce9
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-15 15:44:34 +0200

    move Project class from endpoints.projects module into db.projects module

---
+++ docaudit/db/projects.py
@@ -0,0 +1,77 @@
+# Copyright (C) 2023 Helmar Hutschenreuter
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Any
+
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from ..models import ProjectInput
+from .connection import get_session
+from .operations import delete_from_db, modify_query, read_from_db
+from .schemas import Project
+
+
+class Projects:
+    def __init__(self, session: Session = Depends(get_session)):
+        self.session = session
+
+    def list_projects(
+        self,
+        where_clauses: list[Any] | None = None,
+        order_by_clauses: list[Any] | None = None,
+        offset: int | None = None,
+        limit: int | None = None,
+    ) -> list[Project]:
+        # Construct projects query
+        query = modify_query(
+            select(Project),
+            where_clauses,
+            order_by_clauses or [Project.id],
+            offset,
+            limit,
+        )
+
+        # Execute query
+        return self.session.execute(query).scalars().all()
+
+    def create_project(self, creation: ProjectInput, flush: bool = True) -> Project:
+        project = Project(**creation.dict())
+        self.session.add(project)
+        if flush:
+            self.session.flush()
+        return project
+
+    def get_project(self, project_id: int) -> Project:
+        return read_from_db(self.session, Project, project_id)
+
+    def update_project(
+        self,
+        project: Project,
+        update: ProjectInput,
+        patch: bool = False,
+        flush: bool = True,
+    ) -> None:
+        # Update project
+        for key, value in update.dict(exclude_unset=patch).items():
+            setattr(project, key, value)
+
+        # Flush session
+        if flush:
+            self.session.flush()
+
+    def delete_project(self, project: Project, flush: bool = True) -> None:
+        return delete_from_db(self.session, project, flush)
--- docaudit/endpoints/projects.py
+++ docaudit/endpoints/projects.py
@@ -13,69 +13,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Any
-
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
-from sqlalchemy.orm import Session
 
-from ..db.connection import get_session
-from ..db.operations import delete_from_db, modify_query, read_from_db
+from ..db.projects import Projects
 from ..db.schemas import Project
 from ..models import ProjectInput, ProjectOutput
-
-
-class Projects:
-    def __init__(self, session: Session = Depends(get_session)):
-        self.session = session
-
-    def list_projects(
-        self,
-        where_clauses: list[Any] | None = None,
-        order_by_clauses: list[Any] | None = None,
-        offset: int | None = None,
-        limit: int | None = None,
-    ) -> list[Project]:
-        # Construct projects query
-        query = modify_query(
-            select(Project),
-            where_clauses,
-            order_by_clauses or [Project.id],
-            offset,
-            limit,
-        )
-
-        # Execute query
-        return self.session.execute(query).scalars().all()
-
-    def create_project(self, creation: ProjectInput, flush: bool = True) -> Project:
-        project = Project(**creation.dict())
-        self.session.add(project)
-        if flush:
-            self.session.flush()
-        return project
-
-    def get_project(self, project_id: int) -> Project:
-        return read_from_db(self.session, Project, project_id)
-
-    def update_project(
-        self,
-        project: Project,
-        update: ProjectInput,
-        patch: bool = False,
-        flush: bool = True,
-    ) -> None:
-        # Update project
-        for key, value in update.dict(exclude_unset=patch).items():
-            setattr(project, key, value)
-
-        # Flush session
-        if flush:
-            self.session.flush()
-
-    def delete_project(self, project: Project, flush: bool = True) -> None:
-        return delete_from_db(self.session, project, flush)
-
 
 router = APIRouter(tags=["projects"])
 
